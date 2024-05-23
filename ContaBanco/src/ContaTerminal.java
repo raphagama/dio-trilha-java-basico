f4b9d2b61ba39d4f10e03d5de3ea520b3254fe83
Raphael Gama <raphaelgama19@gmail.com>
2024-05-23 13:30:34 -0300

    Escrita da lógica de todo programa

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         //TODO:Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
 
         //Exibir as mensagens para o usuário
+        System.out.println("Por favor, inserir seu nome: ");
+        String nome  = scanner.nextLine();
 
-        //Obter pela scanner os valores digitados no terminal
+        System.out.println("Por favor, inserir número de conta: ");
+        String conta = scanner.nextLine();
 
-        //Exibir a mensagem conta criada
+        System.out.println("Por favor, inserir número de agencia: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, inserir valor de deposito: ");
+        double valor = scanner.nextDouble();
+
+        System.out.println("Olá, " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + conta + " e seu valor R$" + valor + " já está disponível para saque");
     }
 }
